445ac835dfd9ae5f440de3e458e8a3476bb086a5
Tiago <tvhollowknight@gmail.com>
2024-12-12 17:16:34 -0300

    Fixed the random word generator
    Computer now correctly chooses a random 5-12 letter word

--- game.rb
+++ game.rb
@@ -1,6 +1,6 @@
 class Game
   def initialize
-    words = File.read('google-10000-english-no-swears')
-    @secret_word = words.filter { |word| word.length >= 5 && word.length <= 12 }.sample
+    words = File.open('google-10000-english-no-swears.txt', 'r')
+    @secret_word = words.filter { |word| word.length >= 5 && word.length <= 12 }.sample.chomp
   end
 end
