11d3097a9626a8fe9cd9c319db64c5329892bc72
Tiago <tvhollowknight@gmail.com>
2024-12-20 17:31:33 -0300

    Added the hangman images to a separate file and made the guess function
    Game class now has a guess function and the hangman arts

--- game.rb
+++ game.rb
@@ -1,6 +1,19 @@
 class Game
+  attr_reader :secret_word, :hangmanpics
+
   def initialize
     words = File.open('google-10000-english-no-swears.txt', 'r')
-    @secret_word = words.filter { |word| word.length >= 5 && word.length <= 12 }.sample.chomp
+    @secret_word = words.filter { |word| word.length >= 6 && word.length <= 13 }
+    @hangmanpics = File.read('hangmanpics.txt').split(',')
+    @mistake_count = 0
+  end
+
+  def self.guess(guessed_letter)
+    if @secret_word.split('').include?(guessed_letter)
+      @correct_letters << guessed_letter
+    else
+      @mistake_count += 1
+      @wrong_letters << guessed_letter
+    end
   end
 end
